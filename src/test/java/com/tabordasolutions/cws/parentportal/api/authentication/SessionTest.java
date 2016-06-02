d154a67909cc3c452d8d2f98f2259d620e844236
Tom Parker <tom.parker@tobordasolutions.com>
2016-06-01 22:35:36 -0700

    Refactor session test to make params more readable

--- src/test/java/com/tabordasolutions/cws/parentportal/api/authentication/SessionTest.java
+++ src/test/java/com/tabordasolutions/cws/parentportal/api/authentication/SessionTest.java
@@ -6,31 +6,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SessionTest {
+    private boolean validLogin = true;
+    private boolean inValidLogin = false;
+    String token = "token";
+    String emptyToken = "";
 
     @Test
     public void sessionSuccessfulyCreated(){
-        Session session = new Session(true, "token");
+        Session session = new Session(validLogin, token);
         assertTrue(session.isSuccess());
     }
 
     @Test
     public void sessionNotSuccessfulyCreated(){
-        Session session = new Session(false, "token");
+        Session session = new Session(inValidLogin, token);
         assertFalse(session.isSuccess());
     }
 
     @Test
     public void successfulSessionContainsToken(){
-        String token = "token";
-        Session session = new Session(true, token);
+        Session session = new Session(validLogin, token);
         assertEquals(session.getToken(), token);
     }
 
     @Test
     public void unSuccessfulSessionDoesNotContainsToken(){
-        String token = "token";
-        String emptyToken = "";
-        Session session = new Session(false, token);
+        Session session = new Session(inValidLogin, token);
         assertEquals(session.getToken(), emptyToken);
     }
 }
