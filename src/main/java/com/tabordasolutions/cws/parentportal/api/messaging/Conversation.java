e9ebddc19d7be1378e4e2c240429acae12953f88
Tom Parker <tom.parker@tobordasolutions.com>
2016-06-03 22:19:04 -0700

    Add a conversation object to hold messages

---
+++ src/main/java/com/tabordasolutions/cws/parentportal/api/messaging/Conversation.java
@@ -0,0 +1,88 @@
+package com.tabordasolutions.cws.parentportal.api.messaging;
+
+import java.util.Date;
+import java.util.List;
+
+public class Conversation {
+    private String initializer;
+    private String sender;
+    private String receiver;
+    Date dateCreated;
+    Date dateUpdated;
+    String subject;
+    Message baseMessage;
+    boolean read;
+    List<Message> messages;
+
+    public String getInitializer() {
+        return initializer;
+    }
+
+    public void setInitializer(String initializer) {
+        this.initializer = initializer;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Message getBaseMessage() {
+        return baseMessage;
+    }
+
+    public void setBaseMessage(Message baseMessage) {
+        this.baseMessage = baseMessage;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+}
