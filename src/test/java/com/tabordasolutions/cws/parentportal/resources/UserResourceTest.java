98152241d29e3ffcdef5a25face4c1b7dc20558c
Chris O'Meara <chris@coderhythm.io>
2016-06-05 12:07:43 -0700

    Rework user resource test based on interactions

--- src/test/java/com/tabordasolutions/cws/parentportal/resources/UserResourceTest.java
+++ src/test/java/com/tabordasolutions/cws/parentportal/resources/UserResourceTest.java
@@ -1,19 +1,31 @@
 package com.tabordasolutions.cws.parentportal.resources;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.tabordasolutions.cws.parentportal.api.UserDAO;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.tabordasolutions.cws.parentportal.api.User;
 import com.tabordasolutions.cws.parentportal.services.UserService;
 
 public class UserResourceTest {
-    @Test
-    public void testRetieveUserInfo(){
-        UserResource resource = new UserResource(new UserService(null));
-        User user = resource.user(5L);
-        assertEquals("expected to receive our user firstname", "Fred", user.getFirstName());
-        assertEquals("expected to receive our user lastname", "Flinstone", user.getLastName());
+    private UserService service;
+    private final User user = mock(User.class);
+
+    @Before
+    public void setup() {
+        UserDAO dao = mock(UserDAO.class);
+        when(dao.find(1)).thenReturn(user);
+        when(dao.findByUsername("joey.doe@example.com")).thenReturn(user);
+        service = new UserService(dao);
     }
 
+    @Test
+    public void userReturnsUserObject() {
+        UserResource resource = new UserResource(service);
+        assertEquals(user, resource.user(1));
+    }
 }
